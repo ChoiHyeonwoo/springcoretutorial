3f07a6fbb711890298be24c8c849ad2fe3de3ce1
ChoiHyeonwoo <escape0619@gmail.com>
2021-03-05 09:15:28 +0900

    Spring으로의 전환 (Spring Container를 활용한 의존관계주입)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,23 +7,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-    // 역할과 구현이 모두 드러남.
+
+    /**
+     * 역할과 구현이 드러나게끔 변경
+     * @return
+     */
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        /*return new FixDiscountPolicy();*/
         return new RateDiscountPolicy();
     }
 

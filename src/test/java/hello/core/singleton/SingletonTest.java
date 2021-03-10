b9e110f98c0abe3953014d3e3c65750502a91cd2
ChoiHyeonwoo <escape0619@gmail.com>
2021-03-10 09:13:33 +0900

    싱글턴 적용(장점못지않게 단점이 너무 크리티컬한 패턴)

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -23,4 +23,18 @@ public class SingletonTest {
         // memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //isSameAs : ==으로 비교, isEqualTo: .equals()로 비교
+    }
+
 }

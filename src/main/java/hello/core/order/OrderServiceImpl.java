c942f5412741b34ab9cece7088db5c5c80749542
ChoiHyeonwoo <escape0619@gmail.com>
2021-03-17 16:05:28 +0900

    Custom Annotation

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,18 +1,22 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor// final이 붙은 멤버변수의 생성자를 만들어줌 , 생성자가 하나일 때는 생성자 주입(@Autowired)이 자동 적용된다.
 public class OrderServiceImpl implements OrderService {
 
     // DIP OCP 원칙 준수
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    // final이 붙은 멤버변수의 생성자를 만들어줌 , 생성자가 하나일 때는 생성자 주입(@Autowired)이 자동 적용된다.
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

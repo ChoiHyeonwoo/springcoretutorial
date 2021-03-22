40a2f3b1243bdb6a91e554023aa5bd606dd63493
ChoiHyeonwoo <escape0619@gmail.com>
2021-03-22 11:29:43 +0900

    ObjectProvider[스프링에 의존], JSR-330 Provider[기본 자바 라이브러리] (거의 사용하지 않음 - 거의 싱글톤으로 해결 가능.)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,15 +33,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성 시점에 주입
+
+        private final Provider<PrototypeBean> prototypeBeanObjectProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
